4fe42d2d0243a606ebf60e67a2cf501ea9ca9694
edtsoi <edtsoi@umich.edu>
2018-09-09 17:00:08 -0400

    first commit

---
+++ Assets/Scripts/Column.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Column : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.GetComponent<Bird> () != null)
+        {
+
+            // GameControl.instance.Bird2Scored();
+            if (other.CompareTag("B1"))
+            {
+                GameControl.instance.BirdScored();
+            }
+            if (other.CompareTag("B2"))
+            {
+                GameControl.instance.Bird2Scored();
+            }
+        }
+    }
+}
